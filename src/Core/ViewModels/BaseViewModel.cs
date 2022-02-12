5d5955fa38dc10d10280d28df8d39441c86edb5c
Mosalev Daniel <mosalev.daniel@gmail.com>
2022-02-12 18:25:16 +0700

    Solved task

---
+++ src/Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Core.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Events
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #endregion
+
+        #region Protected Methods
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿namespace Core.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Fields
+
+        private string a;
+        private string b;
+        private string c;
+
+        private bool isValid;
+
+        #endregion
+
+        #region Properties
+
+        public string A
+        {
+            get { return a; }
+            set
+            {
+                a = value;
+                IsValid = CheckSquareCondition();
+                OnPropertyChanged();
+            }
+        }
+
+        public string B
+        {
+            get { return b; }
+            set
+            {
+                b = value;
+                IsValid = CheckSquareCondition();
+                OnPropertyChanged();
+            }
+        }
+
+        public string C
+        {
+            get { return c; }
+            set
+            {
+                c = value;
+                IsValid = CheckSquareCondition();
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsValid
+        {
+            get { return isValid; }
+            set
+            {
+                isValid = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool CheckSquareCondition()
+        {
+            float numberA, numberB, numberC;
+            if (!float.TryParse(a, out numberA) || 
+                !float.TryParse(b, out numberB) || 
+                !float.TryParse(c, out numberC)
+                )
+            {
+                return false;
+            }
+
+            if(numberA * numberA + numberB * numberB == numberC * numberC)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+
+    }
+}
